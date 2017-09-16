1cee01613a55528aa4089983f3446b5ab9858cd0
ankit <emtechsol17@gmail.com>
2017-09-16 17:20:15 +0400

    FIRST PROJECT

---
+++ UserLogin/loginapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from loginapp import views
+
+app_name = 'loginapp'
+
+urlpatterns=[
+
+    url(r'user_login/$',views.user_login,name='user_login'),
+    url(r'^register/', views.register,name = 'register')
+
+]
